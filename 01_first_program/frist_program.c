501cc954aadf9429d4090b7761d664a4300f8ff7
Christopher-Imhoff <cimhoff2709@floridapoly.edu>
2026-05-21 15:09:23 +0000

    lesson one

---
+++ 01_first_program/frist_program.c
@@ -0,0 +1,23 @@
+
+/*
+My first C program
+I will finish it on wednesday
+
+*/
+/*multiline comment*/
+
+// one-line comment
+
+
+//preprocessor directive(starts with #)
+// stdio - standard input output
+//.h - header file
+#include <stdio.h>
+
+// create the main function 
+// <return type> <name> (<parameters>)
+int main(void) {
+    printf("Welcome\nto\nCOP2271!\n"); // \n - new line (escape character)
+    printf("Are\nyou\nready\nto\ncode?\nSpartan!\n");
+    return 0;
+}
